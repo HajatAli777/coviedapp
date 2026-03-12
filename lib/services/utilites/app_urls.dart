da6f8bfb498ea92d47b989221157f48b26944012
Hajat Ali <hajatbagoro@gmail.com>
2026-03-12 13:45:10 -0700

    apis

---
+++ lib/services/utilites/app_urls.dart
@@ -0,0 +1,9 @@
+class AppUrls {
+  //base url for api calls
+  static const String baseUrl = 'https://disease.sh/v3/covid-19/';
+
+  //fetch world covid data
+  static const String worldStatesApi = baseUrl + "all";
+  static const String countriesListApi = baseUrl + "countries";
+  
+}
